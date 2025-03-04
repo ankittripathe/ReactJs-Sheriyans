a7981bb11a61f0634efc7ea5dbb105ac9853bc4e
Dhoni <ankitdhonee@gmail.com>
2025-03-04 22:32:33 +0545

    form handler

--- 05_Form-Handler/src/App.jsx
+++ 05_Form-Handler/src/App.jsx
@@ -16,12 +16,12 @@ const App = () => {
             setUsername(e.target.value);
             // console.log(e.target.value);
           }}
-          className="px-4 py-3 rounded text-xl m-5 text-black"
+          className="px-2 py-1 rounded text-xl m-5 text-black"
           type="text"
           placeholder="Enter Your Name"
         />
 
-        <button className="px-5 py-3 text-xl bg-green-600 text-white rounded">
+        <button className="px-4 py-1 text-xl bg-green-600 text-white rounded">
           Submit
         </button>
       </form>
