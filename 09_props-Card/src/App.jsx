010c1d99a524a0236a9e5807144bf1c4a1bf78dc
Dhoni <ankitdhonee@gmail.com>
2025-03-05 19:38:56 +0545

    props Card

--- 09_props-Card/src/App.jsx
+++ 09_props-Card/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
       profilePhoto:
         "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSLoBEMGqaqYRvbC4_C3GN28Jt49F2PAPo2LOq9eYEIAirrTZhSLKzAPVbr3lHWzViRMyt9waAwSIFPoy0d2s57-A",
     },
+
     {
       name: "Michael Smith",
       age: 34,
@@ -19,6 +20,7 @@ const App = () => {
       profilePhoto:
         "https://plus.unsplash.com/premium_photo-1672239496290-5061cfee7ebb?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
+
     {
       name: "Priya Sharma",
       age: 27,
@@ -27,6 +29,7 @@ const App = () => {
       profilePhoto:
         "https://media.istockphoto.com/id/1278659427/photo/young-woman-sitting-at-sofa-stock-photo.jpg?s=2048x2048&w=is&k=20&c=q0hpSwCS0juhwNo4tVLW3AKpq-RyRm5axU74WYebowM=",
     },
+
     {
       name: "Liam Brown",
       age: 41,
@@ -35,6 +38,7 @@ const App = () => {
       profilePhoto:
         "https://images.unsplash.com/photo-1519058082700-08a0b56da9b4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
+
     {
       name: "Sophia Gonzalez",
       age: 23,
--- 09_props-Card/src/components/Card.jsx
+++ 09_props-Card/src/components/Card.jsx
@@ -5,30 +5,26 @@ const Card = (props) => {
 
   const [add, setAdd] = useState("Add Friend");
 
-  function clickHandler() {
+  const clickHandler = () => {
     setAdd("Remove Friend");
-  }
+  };
 
   return (
     <div className="mr-8 bg-white text-black inline-block text-center rounded p-6">
       <img
-        className="bg-red-400 h-40 w-40 rounded-full mb-4 m-auto"
+        className="bg-red-400 h-40 w-40 rounded-full m-auto"
         src={props.photo}
         alt="Profile_Picture"
       />
 
-      <h1 className="text-2xl font-semibold mb-2">
-        {props.user}
-      </h1>
+      <h1 className="text-2xl font-semibold mb-2">{props.user}</h1>
 
       <h2 className="font-medium">
         {props.city}, {props.age}
       </h2>
 
-      <h2 className="font-medium">
-        {props.prof}
-       </h2>
-       
+      <h2 className="font-medium">{props.prof}</h2>
+
       <button
         className="bg-blue-600 text-white px-4 py-2 rounded font-medium mt-3"
         onClick={clickHandler}
