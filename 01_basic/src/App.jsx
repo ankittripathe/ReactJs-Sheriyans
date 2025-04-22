98d08fec5ea08e7458568fedc4e513cbda914e44
Dhoni <ankitdhonee@gmail.com>
2025-04-22 15:19:22 +0545

    basic

--- 01_basic/src/App.jsx
+++ 01_basic/src/App.jsx
@@ -5,9 +5,9 @@ const App = () => {
   let age = 25;
 
   const handleChange = () => {
-    console.log(user);
-    user = "Aryan"; // Real DOM se intract ho rha hai na ki Virtual DOM. So UI pe change nhi hoga.
-    console.log(user);
+    console.log(user);  
+    user = "Aryan";    // Real DOM se intract ho rha hai na ki Virtual DOM. So UI pe change nhi hoga.
+    console.log(user);  
   };
 
   return (
